2e5918a7e182fc58d803abc261754aa45db5397a
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-09-29 07:34:35 +0300

    Handle submit

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,8 +12,19 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(e){
+    e.preventDefault(); //stops the browser from refreshing
+    const formData = {
+      firstName: firstName,
+      lastName: lastName
+    }; //putting togethert the current form data into an object using the values stored in state
+    props.sendFormDataSomewhere (formData); //send to server or another page
+    setFirstName(''); //on javascript it would be e.target.reset()
+    setLastName(''); //setting state to an empty string clears data once the form has been submitted
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
