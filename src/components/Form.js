aba82032c129f8bff844c727c134b2e1b8a7969d
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-09-29 07:43:11 +0300

    Functioning controlled form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState ([]) //method 2
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -18,17 +19,31 @@ function Form(props) {
       firstName: firstName,
       lastName: lastName
     }; //putting togethert the current form data into an object using the values stored in state
-    props.sendFormDataSomewhere (formData); //send to server or another page
+    //props.sendFormDataSomewhere (formData); //send to server or another page //method 1
+    const dataArray = [...submittedData, formData]; //method 2
+    setSubmittedData(dataArray); //method 2
     setFirstName(''); //on javascript it would be e.target.reset()
     setLastName(''); //setting state to an empty string clears data once the form has been submitted
   }
-
+  //method 2 start
+  const listOfSubmissions = submittedData.map((data, index) =>{
+    return (
+      <div key= {index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+  //method 2 close
   return (
+    <div>
     <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+    <h3>Submissions</h3>
+    {listOfSubmissions}
+    </div>
   );
 }
 
